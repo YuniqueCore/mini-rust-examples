673a034fad036c71135365688b48386ce698501f
unic <yuniqueunic@gmail.com>
2025-10-07 01:08:45 +0800

    feat(en-de): add encryption logic with xor cipher and error handling
    - Introduce `anyverr` crate for error handling and define custom Result type
    - Implement basic XOR encryption logic with span-based skipping
    - Add initial structure for ChaCha20Poly1305 and RC6 encryption methods
    - Include unit tests for XOR encryption functionality
    - Update Cargo.lock and Cargo.toml to include new dependency `anyverr`

--- crates/crypto-net/en-de/src/lib.rs
+++ crates/crypto-net/en-de/src/lib.rs
@@ -1,5 +1,7 @@
+use anyverr::{AnyError, AnyResult};
 use chacha20poly1305::{AeadCore, ChaCha20Poly1305, KeyInit, aead::OsRng};
 
+type Result<T> = AnyResult<T>;
 type Span = u16;
 
 pub enum Cipher {
@@ -9,31 +11,54 @@ pub enum Cipher {
 }
 
 impl Cipher {
-    pub fn encrypt(&self, data: &[u8], key: &[u8], nonce: Option<&[u8]>) {
-        let CryptoSuite { key, nonce } = CryptoSuite::new();
-        let key = &key.as_slice();
-        if let Some(n) = nonce {
-            let nonce = Some(n.as_slice());
-            match self {
-                Cipher::Xor(span) => Self::encode_with_xor(data, span, key, nonce),
-                Cipher::ChaCha20Poly1305 => Self::encode_with_chacha20(data, key, nonce),
-                Cipher::Rc6 => Self::encode_with_rc6(data, key, nonce),
-            }
-        } else {
-            let nonce = None;
-            match self {
-                Cipher::Xor(span) => Self::encode_with_xor(data, span, key, nonce),
-                Cipher::ChaCha20Poly1305 => Self::encode_with_chacha20(data, key, nonce),
-                Cipher::Rc6 => Self::encode_with_rc6(data, key, nonce),
-            }
+    pub fn encrypt(&self, data: &[u8], key: &[u8], nonce: Option<&[u8]>) -> Result<Vec<u8>> {
+        match self {
+            Cipher::Xor(span) => Self::encrypt_xor(data, span, key, nonce),
+            Cipher::ChaCha20Poly1305 => Self::encrypt_chacha20(data, key, nonce),
+            Cipher::Rc6 => Self::encrypt_rc6(data, key, nonce),
         }
     }
 
-    pub fn decrypt(&self, data: &[u8], key: &[u8], nonce: Option<&[u8]>) {}
+    pub fn decrypt(&self, data: &[u8], key: &[u8], nonce: Option<&[u8]>) -> Result<Vec<u8>> {
+        Ok(vec![])
+    }
 
-    fn encode_with_xor(data: &[u8], span: &Option<Span>, key: &[u8], nonce: Option<&[u8]>) {}
-    fn encode_with_chacha20(data: &[u8], key: &[u8], nonce: Option<&[u8]>) {}
-    fn encode_with_rc6(data: &[u8], key: &[u8], nonce: Option<&[u8]>) {}
+    fn encrypt_xor(
+        data: &[u8],
+        span: &Option<Span>,
+        key: &[u8],
+        _nonce: Option<&[u8]>,
+    ) -> Result<Vec<u8>> {
+        if key.is_empty() {
+            return Err(AnyError::quick(
+                "Key is empty",
+                anyverr::ErrKind::ValueValidation,
+            ));
+        }
+
+        let mut res = Vec::with_capacity(data.len());
+        if let Some(s) = *span {
+            for (i, d) in data.iter().enumerate() {
+                if i % s as usize == 0 {
+                    // skip this bit which is n*span
+                    res.push(*d);
+                    continue;
+                }
+
+                let k = key[i % key.len()];
+                let xor = d ^ k;
+                res.push(xor);
+            }
+        }
+
+        Ok(res)
+    }
+    fn encrypt_chacha20(data: &[u8], key: &[u8], nonce: Option<&[u8]>) -> Result<Vec<u8>> {
+        Ok(vec![])
+    }
+    fn encrypt_rc6(data: &[u8], key: &[u8], nonce: Option<&[u8]>) -> Result<Vec<u8>> {
+        Ok(vec![])
+    }
 }
 
 struct CryptoSuite {
@@ -56,5 +81,47 @@ impl CryptoSuite {
         let nonce = Some(nonce.to_vec());
 
         CryptoSuite { key, nonce }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_xor() -> Result<()> {
+        let data = String::from("Hello world");
+        let CryptoSuite { key, nonce } = CryptoSuite::new();
+        let key = &key.as_slice();
+        // let mut os_rng = OsRng::default();
+        // let random_span = os_rng.next_u32() as usize / data.len();
+        let xor_cipher = Cipher::Xor(Some(2));
+        println!("origin: {:?}", data.as_bytes());
+        println!("origin msg: {}", data);
+        let res = if let Some(n) = nonce.clone() {
+            let nonce = Some(n.as_slice());
+            xor_cipher.encrypt(data.as_bytes(), key, nonce)?
+        } else {
+            let nonce = None;
+            xor_cipher.encrypt(data.as_bytes(), key, nonce)?
+        };
+        println!("xor: {:?}", res);
+        let msg = String::from_utf8_lossy(&res);
+        println!("xor msg: {}", msg);
+
+        let xor_cipher = Cipher::Xor(Some(3));
+
+        let res = if let Some(n) = nonce {
+            let nonce = Some(n.as_slice());
+            xor_cipher.encrypt(&res, key, nonce)?
+        } else {
+            let nonce = None;
+            xor_cipher.encrypt(&res, key, nonce)?
+        };
+        println!("xor2: {:?}", res);
+        let msg = String::from_utf8_lossy(&res);
+        println!("xor2 msg: {}", msg);
+
+        Ok(())
     }
 }
