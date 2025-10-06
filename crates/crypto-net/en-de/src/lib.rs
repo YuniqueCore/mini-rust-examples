d47e98bb1e1c3568ea45ab095796ac1078356a18
unic <yuniqueunic@gmail.com>
2025-10-06 23:15:39 +0800

    feat(crypto-net): implement key and nonce generation for encryption
    - Add dependencies for ChaCha20Poly1305 and RNG utilities
    - Introduce `CryptoSuite` struct to handle key and nonce generation
    - Modify `encrypt` method to use generated key and nonce
    - Update parameter order in `encode_with_xor` for consistency
    - Add basic structure for key/nonce generation using OS RNG

--- crates/crypto-net/en-de/src/lib.rs
+++ crates/crypto-net/en-de/src/lib.rs
@@ -1,3 +1,8 @@
+use chacha20poly1305::{
+    AeadCore, ChaCha20Poly1305, KeyInit,
+    aead::{OsRng, rand_core::RngCore},
+};
+
 type Span = u16;
 
 pub enum Cipher {
@@ -8,16 +13,51 @@ pub enum Cipher {
 
 impl Cipher {
     pub fn encrypt(&self, data: &[u8], key: &[u8], nonce: Option<&[u8]>) {
-        match self {
-            Cipher::Xor(span) => Self::encode_with_xor(data, key, nonce, span),
-            Cipher::ChaCha20Poly1305 => Self::encode_with_chacha20(data, key, nonce),
-            Cipher::Rc6 => Self::encode_with_rc6(data, key, nonce),
+        let CryptoSuite { key, nonce } = CryptoSuite::generate_key_nonce();
+        let key = &key.as_slice();
+        if let Some(n) = nonce {
+            let nonce = Some(n.as_slice());
+            match self {
+                Cipher::Xor(span) => Self::encode_with_xor(data, span, key, nonce),
+                Cipher::ChaCha20Poly1305 => Self::encode_with_chacha20(data, key, nonce),
+                Cipher::Rc6 => Self::encode_with_rc6(data, key, nonce),
+            }
+        } else {
+            let nonce = None;
+            match self {
+                Cipher::Xor(span) => Self::encode_with_xor(data, span, key, nonce),
+                Cipher::ChaCha20Poly1305 => Self::encode_with_chacha20(data, key, nonce),
+                Cipher::Rc6 => Self::encode_with_rc6(data, key, nonce),
+            }
         }
     }
 
     pub fn decrypt(&self, data: &[u8], key: &[u8], nonce: Option<&[u8]>) {}
 
-    fn encode_with_xor(data: &[u8], key: &[u8], nonce: Option<&[u8]>, span: &Option<Span>) {}
+    fn encode_with_xor(data: &[u8], span: &Option<Span>, key: &[u8], nonce: Option<&[u8]>) {}
     fn encode_with_chacha20(data: &[u8], key: &[u8], nonce: Option<&[u8]>) {}
     fn encode_with_rc6(data: &[u8], key: &[u8], nonce: Option<&[u8]>) {}
+}
+
+struct CryptoSuite {
+    key: Vec<u8>,
+    nonce: Option<Vec<u8>>,
+}
+
+impl CryptoSuite {
+    pub fn new() -> Self {
+        Self::generate_key_nonce()
+    }
+
+    fn generate_key_nonce() -> CryptoSuite {
+        let os_rng = OsRng::default();
+
+        let key = ChaCha20Poly1305::generate_key(os_rng);
+        let key = key.to_vec();
+
+        let nonce = ChaCha20Poly1305::generate_nonce(os_rng);
+        let nonce = Some(nonce.to_vec());
+
+        CryptoSuite { key, nonce }
+    }
 }
