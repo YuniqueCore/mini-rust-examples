8eee344a3219d10c26f8fab014a6564c94690911
unic <yuniqueunic@gmail.com>
2025-12-28 15:55:34 +0800

    feat(httpserver): add command line argument parsing with sarge
    - Add anyhow, fern, and mea dependencies to Cargo.lock
    - Integrate sarge crate for command line argument parsing
    - Implement Args struct with bind address and serve path options
    - Add BindAddr and ServePath wrapper types with ArgumentType implementations
    - Create deref macros for convenient type handling
    - Set up basic module structure in lib.rs

---
+++ crates/httpserver/src/cmd/mod.rs
@@ -0,0 +1,92 @@
+
+use std::{net::SocketAddr, ops::{Deref, DerefMut}, path::PathBuf, str::FromStr};
+
+use sarge::{ArgumentType, prelude::*};
+
+
+sarge! {
+    #[derive(Debug)]
+    pub Args,
+
+    /// The bind addr for serving.
+    #ok 'l' pub bind:BindAddr = BindAddr::from_str("127.0.0.1:8080").unwrap(),
+
+    /// the dir/file will be served
+    #ok 's' pub serve: ServePath = ServePath::from_str(".").unwrap(),
+}
+
+
+macro_rules! impl_deref_mut {
+    (
+        $struct:ident ( $target:ident )
+    ) => {
+        
+        impl Deref for $struct {
+            type Target = $target;
+
+            fn deref(&self) -> &Self::Target {
+                &self.0
+            }
+        }
+
+        impl DerefMut for $struct {
+            fn deref_mut(&mut self) -> &mut Self::Target {
+                &mut self.0
+            }
+        }
+
+    };
+}
+
+#[derive(Debug)]
+pub struct  BindAddr(SocketAddr);
+
+impl ArgumentType for BindAddr {
+    type Error=ArgParseError;
+
+    fn from_value(val: Option<&str>) -> sarge::ArgResult<Self> {
+       if let Some(v) = val {
+            let bind_addr  = SocketAddr::from_str(v).ok()?;
+            return Ok(BindAddr(bind_addr)).into();
+       }
+        None
+    }
+}
+
+impl FromStr for  BindAddr {
+    type Err=std::net::AddrParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+         Ok(Self(SocketAddr::from_str(s)?))
+    }
+}
+
+impl_deref_mut!(BindAddr(SocketAddr));
+
+
+#[derive(Debug)]
+pub struct  ServePath(PathBuf);
+
+impl ArgumentType for ServePath {
+    type Error=ArgParseError;
+
+    fn from_value(val: Option<&str>) -> sarge::ArgResult<Self> {
+       if let Some(v) = val {
+            let path  =PathBuf::from_str(v).unwrap();
+            if !path.exists(){
+                return  None;
+            }
+            return Ok(ServePath(path)).into();
+       }
+        None
+    }
+}
+
+impl FromStr for ServePath {
+    type Err = core::convert::Infallible;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self(PathBuf::from_str(s)?))
+    }
+}
+
+impl_deref_mut!(ServePath(PathBuf));
